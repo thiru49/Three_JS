f8f2c6ad6792500fd6a910939ee737f2097e91f7
thiru49 <thiruppathi497@gmail.com>
2023-12-30 15:23:52 +0530

    transform 3d onject

--- Transform_Object/src/script.js
+++ Transform_Object/src/script.js
@@ -1,14 +1,13 @@
 import * as THREE from 'three'
 
 // Canvas
-
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
 
 /**
- * Object
+ * Objects
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
